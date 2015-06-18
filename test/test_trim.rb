718d3256215469940ae31174babe8186abd73d43
sanemat <o.gata.ken@gmail.com>
2015-06-19 02:26:18 +0900

    chore(trim): shorten variable name

--- test/test_trim.rb
+++ test/test_trim.rb
@@ -4,16 +4,16 @@ module RestoreBundledWith
   class TestTrim < Test::Unit::TestCase
     sub_test_case '.trim' do
       test 'v1.9 lock file' do
-        actual_lock_file = File.read('./test/fixtures/v1-9-example1.lock')
+        lock_file = File.read('./test/fixtures/v1-9-example1.lock')
         assert do
-          Trim.new(actual_lock_file).trim == actual_lock_file
+          Trim.new(lock_file).trim == lock_file
         end
       end
       test 'v1.10 lock file' do
-        actual_lock_file = File.read('./test/fixtures/v1-10-example1.lock')
-        expected_trimmed_lock_file = File.read('./test/fixtures/v1-10-example1-trimmed.lock')
+        lock_file = File.read('./test/fixtures/v1-10-example1.lock')
+        trimmed = File.read('./test/fixtures/v1-10-example1-trimmed.lock')
         assert do
-          Trim.new(actual_lock_file).trim == expected_trimmed_lock_file
+          Trim.new(lock_file).trim == trimmed
         end
       end
     end

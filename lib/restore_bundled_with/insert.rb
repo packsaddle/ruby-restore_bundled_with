54187a5cc30cea9503ea4ed37104ed95afe0f207
sanemat <o.gata.ken@gmail.com>
2015-06-20 03:53:25 +0900

    chore(insert): injectable new line format

--- lib/restore_bundled_with/insert.rb
+++ lib/restore_bundled_with/insert.rb
@@ -1,13 +1,14 @@
 module RestoreBundledWith
   class Insert
-    def initialize(body, section)
+    def initialize(body, section, new_line = "\n")
       @body = body
       @section = section
+      @new_line = new_line
     end
 
     def insert
       if @section && !@section.empty?
-        @body + @section + "\n"
+        @body + @section + @new_line
       else
         @body
       end

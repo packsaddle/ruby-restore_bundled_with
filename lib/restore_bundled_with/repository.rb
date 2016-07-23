88f315f9f36955cc1ec809fe06b8d4318310fcec
Sanemat <o.gata.ken@gmail.com>
2016-07-24 03:30:19 +0900

    chore(repository): use parent method

--- lib/restore_bundled_with/repository.rb
+++ lib/restore_bundled_with/repository.rb
@@ -9,9 +9,7 @@ module RestoreBundledWith
     #
     # @return [String] target file contents
     def fetch_file(file = Lock::FILE_NAME, ref = REF, new_line = NEW_LINE)
-      # NOTE: git.cat_file trims last \n?
-      text = git.cat_file("#{ref}:#{file}")
-      text + new_line
+      super(file, ref, new_line)
     end
   end
 end

01d1ef461244059673893e3035684264ea30196b
Sanemat <o.gata.ken@gmail.com>
2016-07-24 02:02:20 +0900

    docs(repository): add docs

--- lib/restore_bundled_with/repository.rb
+++ lib/restore_bundled_with/repository.rb
@@ -1,4 +1,5 @@
 module RestoreBundledWith
+  # The git repository
   class Repository
     LOCK_FILE = 'Gemfile.lock'.freeze
     REF = 'HEAD'.freeze
@@ -6,15 +7,25 @@ module RestoreBundledWith
     GIT_OPTIONS = {}.freeze
     NEW_LINE = "\n".freeze
 
+    # @param git_path [String] git repository path
+    # @param git_options [Hash] ruby-git options
+    #
+    # @return [Repository] Repository instance
     def initialize(git_path = GIT_PATH, git_options = GIT_OPTIONS)
       @git_path = git_path
       @git_options = git_options
     end
 
+    # @return [Git::Base] ruby-git object
     def git
       @git ||= Git.open(@git_path, @git_options)
     end
 
+    # @param file [String] target file
+    # @param ref [String] git ref
+    # @param new_line [String] file's ending new line
+    #
+    # @return [String] target file contents
     def fetch_file(file = LOCK_FILE, ref = REF, new_line = NEW_LINE)
       # NOTE: git.cat_file trims last \n?
       text = git.cat_file("#{ref}:#{file}")

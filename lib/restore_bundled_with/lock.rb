699ad63d099fa3348b3c7862fb2bf00d356cd889
Sanemat <o.gata.ken@gmail.com>
2016-07-23 23:29:38 +0900

    wip

--- lib/restore_bundled_with/lock.rb
+++ lib/restore_bundled_with/lock.rb
@@ -1,13 +1,5 @@
 module RestoreBundledWith
   class Lock < ::RestoreFromRepository::File
     REGEX_BUNDLED_WITH = /^(?<pick>(?:\r\n|\r|\n)^BUNDLED WITH.*(?:\r\n|\r|\n).+(?:\r\n|\r|\n))/
-
-    def self.insert(text, section)
-      if section && !section.empty?
-        new(text + section)
-      else
-        new(text)
-      end
-    end
   end
 end
